32199591afd3ecaae665f2fad93c2689dcaf5729
Ryan Micael <ka9ddcgamer@gmail.com>
2023-04-23 22:45:41 -0300

    feat: fixes

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -13,7 +13,7 @@ const PostCard = ({ post }) => {
           className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
         />
       </div>
-      <h1 className="transaition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
+      <h1 className="transaition duration-700 text-center mb-8 cursor-pointer hover:text-indigo-600 text-3xl font-semibold">
         <Link href={`/post/${post.slug}`}>{post.title}</Link>
       </h1>
       <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
@@ -32,7 +32,7 @@ const PostCard = ({ post }) => {
         <div className="font-medium text-gray-700">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 inline mr-2 text-pink-500"
+            className="h-6 w-6 inline mr-2 text-indigo-600"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -52,7 +52,7 @@ const PostCard = ({ post }) => {
       </p>
       <div className="text-center">
         <Link href={`/post/${post.slug}`}>
-          <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
+          <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-indigo-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
             Continue Reading...
           </span>
         </Link>
--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -60,54 +60,58 @@ const PostDetail = ({ post }) => {
   };
 
   return (
-    <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
-      <div className="relative overflow-hidden shadow-md mb-6">
+    <div className="bg-white shadow-lg rounded-xl lg:p-4 pb-12 mb-8">
+      <div className="relative overflow-hidden shadow-md mb-6 rounded-xl">
         <img
           src={post.coverImage.url}
           alt={post.title}
-          className="object-top h-full w-full rounded-t-lg"
+          className="object-top h-full w-full rounded-xl"
         />
       </div>
-      <div className="px-4 lg:px-0">
-        <div className="flex items-center mb-8 w-full ">
-          <div className="flex items-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
-            <img
-              alt={post.author.name}
-              height="30px"
-              width="30px"
-              className="align-middle rounded-full"
-              src={post.author.picture.url}
-            />
-            <p className="inline align-middle text-gray-700 ml-2 text-lg">
-              {post.author.name}
-            </p>
-          </div>
-          <div className="font-medium text-gray-700">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 inline mr-2 text-pink-500"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-              />
-            </svg>
-            <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
-          </div>
-        </div>
-        <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
-        {post.content.raw.children.map((typeObj, index) => {
-          const children = typeObj.children.map((item, itemIndex) =>
-            getContentFragment(itemIndex, item.text, item)
-          );
+      <div className="border-indigo-600 shadow-2xl border-t-[3px] border-b-[1px] rounded-t-4xl rounded-b-4xl bg-white relative bottom-24 mx-12 lg:px-0">
+        <h1 className="text-center mb-8 text-3xl font-semibold mt-12">
+          {post.title}
+        </h1>
+        <div className="mx-8 mb-12">
+          {post.content.raw.children.map((typeObj, index) => {
+            const children = typeObj.children.map((item, itemIndex) =>
+              getContentFragment(itemIndex, item.text, item)
+            );
 
-          return getContentFragment(index, children, typeObj, typeObj.type);
-        })}
+            return getContentFragment(index, children, typeObj, typeObj.type);
+          })}
+        </div>
+      </div>
+      <div className="flex items-center mb-8 w-full pl-4">
+        <div className="flex items-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
+          <img
+            alt={post.author.name}
+            height="30px"
+            width="30px"
+            className="align-middle rounded-full"
+            src={post.author.picture.url}
+          />
+          <p className="inline align-middle text-gray-700 ml-2 text-lg">
+            {post.author.name}
+          </p>
+        </div>
+        <div className="font-medium text-gray-700">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 inline mr-2 text-indigo-600"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+            />
+          </svg>
+          <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
+        </div>
       </div>
     </div>
   );
